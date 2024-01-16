af64d4d23d28d8efafc7bcbccab5335cc4b763dd
Ellen Spertus <ellen.spertus@gmail.com>
2024-01-16 12:28:16 -0800

    Add getters and setters

--- src/Clock.java
+++ src/Clock.java
@@ -2,8 +2,10 @@
  * A 24-hour clock managed entirely by the user.
  */
 public class Clock {
-    private static final int MAX_HOUR = 23;
-    private static final int MAX_MINUTE = 59;
+    private static final int NUM_HOURS = 24;
+    private static final int MAX_HOUR = NUM_HOURS - 1;
+    private static final int NUM_MINUTES = 60;
+    private static final int MAX_MINUTE = NUM_MINUTES - 1;
 
     private int hour;
     private int minute;
@@ -15,8 +17,8 @@ public class Clock {
      * @param minute the minute
      */
     public Clock(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
+        this.hour = hour % NUM_HOURS;
+        this.minute = minute % NUM_MINUTES;
     }
 
     /**
@@ -27,12 +29,26 @@ public class Clock {
         minute = 0;
     }
 
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setHour(hour) {
+        this.hour = hour % MAX_HOUR;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute % MAX_MINUTE;
+    }
+
     public static void main(String[] args) {
         System.out.println(Clock.MAX_HOUR);  // 23
 
-        Clock clock1 = new Clock(10, 35);
-        System.out.println(clock1.hour);   // 10
-        Clock clock2 = new Clock();
-        System.out.println(clock2.hour);   // 0
+        Clock clock1 = new Clock(25, 0);
+        System.out.println(clock1.hour);   // 1
     }
 }
